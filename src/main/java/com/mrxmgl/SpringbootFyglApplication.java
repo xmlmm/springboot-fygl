e425d55963dbeab8dbaa9a4dac6d4435f9d1b343
ztf <1050434850@qq.com>
2019-12-15 21:20:05 +0800

    跨服务调用；

--- src/main/java/com/mrxmgl/SpringbootFyglApplication.java
+++ src/main/java/com/mrxmgl/SpringbootFyglApplication.java
@@ -2,10 +2,12 @@ package com.mrxmgl;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients//启用feign客户端
 @SpringBootApplication
+@EnableEurekaClient
 public class SpringbootFyglApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/mrxmgl/controller/BgtzController.java
+++ src/main/java/com/mrxmgl/controller/BgtzController.java
@@ -6,10 +6,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * @author Administrator
- * @date 2019/7/12 0012 09:32
- */
 @RestController
 @RequestMapping(value = "/fygl/bgtz")
 public class BgtzController {
@@ -19,19 +15,18 @@ public class BgtzController {
 
     @RequestMapping(value = "/browse")
     public String browse(){
-        String aa = bgtzService.hiService("你好--");
-        return aa;
+        return bgtzService.hiService("你好--");
     }
 
-    @RequestMapping(value = "/index")
-    public String index(Model model) {
-        System.out.println("index------");
-        return "index";
+    /**
+     * admin服务测试调用此方法
+     * @param name
+     * @return
+     */
+    @RequestMapping(value = "/testFeign")
+    public String testFeign(String name){
+        System.out.println("testFeign：调用成功");
+        return name+"--------------调用成功";
     }
 
-    @RequestMapping(value = "/login")
-    public String login(Model model) {
-        System.out.println("login------");
-        return "index";
-    }
 }
